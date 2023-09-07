a824dae190b0e88929b01b85525ea91a22c7317a
mobb99 <mobb99@proton.me>
2023-09-07 15:40:48 -0700

    fixed 101-quote.c write instead of perror

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - print a message to stderr
  *
- * Return: return 1 , its an error
+ * Return: return 1 to signal an error
  */
 
 int main(void)
 {
-	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, msg, 60);
 	return (1);
 }
