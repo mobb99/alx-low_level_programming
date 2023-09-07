9e48a3eb22178a474c3fecbaf1144b05b24802e2
mobb99 <mobb99@proton.me>
2023-09-07 15:10:51 -0700

    added 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print a message to stderr
+ *
+ * Return: return 1 , its an error
+ */
+
+int main(void)
+{
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
