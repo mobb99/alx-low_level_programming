d0abd35b64ecf0cbb4704311855fce8513cdab5d
mobb99 <mobb99@proton.me>
2023-09-07 15:17:20 -0700

    added 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return (0);
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
