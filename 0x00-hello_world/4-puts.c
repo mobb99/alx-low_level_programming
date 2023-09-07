148fe61d13c0a97c7b68e434d53c64b69f3730c2
mobb99 <mobb99@proton.me>
2023-09-07 12:51:15 -0700

    fixed  4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/*
+ * main :entry point 
+ * return 0 if successfull
+ * */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
