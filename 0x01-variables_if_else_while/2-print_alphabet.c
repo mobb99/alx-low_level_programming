118b7212e69e12f444d54e9a612ea72259667b80
mobb99 <mobb99@proton.me>
2023-09-08 10:29:19 -0700

    added 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print the alphabet
+ *
+ * Return: return 0 if success
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		putchar("\n");
+	}
+
+return (0);
+}
