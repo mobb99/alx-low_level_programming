3d18d792aa5dd390961a57677285b3c7b4ece1b4
mobb99 <mobb99@proton.me>
2023-09-12 16:13:03 -0700

    added 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of n
+ * @n: an int n
+ *
+ * Return: the abs of n
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	return (n);
+}
