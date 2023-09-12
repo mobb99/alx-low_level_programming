f30fa5557daa8185c516181e5229fc0f76a506d8
mobb99 <mobb99@proton.me>
2023-09-12 15:58:15 -0700

    added 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of a number
+ * @n: an int n
+ *
+ * Return: 1 if gt 0,0 if 0, -1 if lt 0
+ */
+
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar("-");
+		return -1;
+	}
+	if (n > 0)
+	{
+		_putchar("+");
+		return 1;
+	}
+	_putchar("0");
+	return 0;
+}
