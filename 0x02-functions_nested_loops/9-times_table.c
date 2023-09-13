13a17573470eccd3e1952ffcda36b87bd65f3e02
mobb99 <mobb99@proton.me>
2023-09-12 17:48:04 -0700

    added 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - prints times tables
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int f;
+	int s, r;
+
+	for (f = 0; f < 10; f++)
+	{
+		for (s = 0; s < 10; s++)
+		{
+		if (s != 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		if (f * s < 10)
+		{
+			_putchar(' ');
+			_putchar(f * s + '0');
+		}
+		else
+		_putchar(f * s + '0');
+		}
+		_putchar('\n');
+	}
+}
