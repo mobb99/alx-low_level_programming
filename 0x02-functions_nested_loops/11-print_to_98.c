34e1722c80e0542e890736c614155ae4abe105cf
mobb99 <mobb99@proton.me>
2023-09-12 18:38:58 -0700

    added 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print numbers from n to 98
+ * @n: an int the starting  point
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	int h;
+
+	printf("%d", n);
+	for (h = n; h < 98; h++)
+	{
+		printf(", %d", h);
+	}
+}
