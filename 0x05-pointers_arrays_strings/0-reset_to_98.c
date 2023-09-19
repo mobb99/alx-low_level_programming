387b1c3b6c04e180122ea33ebe2685f952c1e8af
mobb99 <mobb99@proton.me>
2023-09-19 09:38:14 -0700

    added 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - change the vlaue of n to 98
+ * @n : holds the adress of n
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return (void);
+}
