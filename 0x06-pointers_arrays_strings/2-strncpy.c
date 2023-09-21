21e3516343f2bda8a07ff14b8f14e1826834fada
mobb99 <mobb99@proton.me>
2023-09-21 11:43:11 -0700

    added 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - copy atmost n bytes from  src to dest overwriting it
+ * @src : the source string
+ * @dest : the destination
+ * @n : number of bytes to copy
+ *
+ * Return: rhe pointer to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int c = 0;
+
+	while (src[c] != '\0' && c < n)
+	{
+		dest[c] = src[x];
+		c++;
+	}
+	while (c < n)
+	{
+		dest[c] = '\0';
+		c++;
+	}
+	return (dest);
+}
