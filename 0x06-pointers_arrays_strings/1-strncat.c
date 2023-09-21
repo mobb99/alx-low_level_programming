49b132b828cc8e3801cc8e49ca40650d0a1e971c
mobb99 <mobb99@proton.me>
2023-09-21 11:02:09 -0700

    added 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - append atcmost n bytes from  src to dest overwriting (\0)
+ * @src : the source string
+ * @dest : the destination
+ * @n : number of bytes to copy
+ *
+ * Return: rhe pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int c = 0;
+	int x = 0;
+
+	while (dest[c] != '\0')
+	{
+		c++;
+	}
+	while (src[x] != '\0' && x < n - 1)
+	{
+		dest[c] = src[x];
+		c++;
+		x++;
+	}
+	dest[c] = src[x];
+	return (dest);
+}
