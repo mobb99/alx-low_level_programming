69da83df70a2fbf03e1942d40b4fdc515904ad6c
mobb99 <mobb99@proton.me>
2023-09-21 16:39:20 -0700

    added 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - change lower case letters to upper
+ * @str : string to modify
+ *
+ * Return: pointer to str
+ */
+
+char *string_toupper(char *str)
+{
+	int c = 0;
+
+	while (str[c] != '\0')
+	{
+		if (sr[c] <= 'z' && str[] >= 'a')
+			str[c] = str[c] - 'a' + 'A';
+		c++;
+	}
+	return (str);
+}
