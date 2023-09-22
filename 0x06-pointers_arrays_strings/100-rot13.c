4688432dda5d2596e5ec0f908d84f0e5a2af7db7
mobb99 <mobb99@proton.me>
2023-09-21 19:02:18 -0700

    added 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encode a string using rot13
+ * @str : string to modify
+ *
+ * Return: pointer to str
+ */
+
+char *rot13(char *str)
+{
+	int c = 0;
+	char set1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char set2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i;
+
+	while (str[c] != '\0')
+	{
+	for (i = 0; i < 52; i++)
+	{
+		if (str[c] == set1[i])
+			str[c] = set2[i];
+	}
+	c++;
+	}
+	return (str);
+}
