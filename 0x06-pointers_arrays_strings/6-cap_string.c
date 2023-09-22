59547105252a72a8d03888ab70ebd8594d83755f
mobb99 <mobb99@proton.me>
2023-09-21 17:21:43 -0700

    added 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_sep - check if a char is a valid separator
+ * @l : a char
+ *
+ * Return: 1 if true 0 else
+ */
+
+int is_sep(char l)
+{
+char seps[] = "	\n,;	.!?\"(){}";
+int i = 0;
+
+	for (; seps[i] != '\0'; i++)
+	{
+		if (l == seps[i])
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ * cap_string - capitalize every word that follows a separator
+ * @str : string to modify
+ *
+ * Return: pointer to str
+ */
+
+char *cap_string(char *str)
+{
+	int c = 0;
+	int wasep = 1;
+
+	while (str[c] != '\0')
+	{
+		if (str[c] <= 'z' && str[c] >= 'a' && wasep == 1)
+			str[c] = str[c] - 'a' + 'A';
+		if (is_sep(str[c]))
+			wasep = 1;
+		c++;
+	}
+	return (str);
+}
