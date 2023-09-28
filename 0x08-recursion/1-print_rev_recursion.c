99387e64aade69ca23d698d7ab627694432640c5
mobb99 <mobb99@proton.me>
2023-09-28 13:50:31 -0700

    added 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s : the string s
+ *
+ * Return: nothing void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
