3b8acf7855d14f0cc669664eb3100d282413173c
mobb99 <mobb99@proton.me>
2023-10-02 16:57:22 -0700

    added readme,main.h and 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ * @argc : number of arguments passed to the program
+ * @argv : an array made of the arguments
+ *
+ * Return: always 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include<string.h>
+#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+void _putchar(int c);
+#endif
