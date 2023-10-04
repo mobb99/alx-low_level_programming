186dcf1df58a18856eb47b111a88949991d88bf7
mobb99 <mobb99@proton.me>
2023-10-04 14:24:27 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * str_concat - concatenates two strings
+ * @s1 : first string
+ * @s2 : second string
+ *
+ * Return: a pointer to the string or null if it fails
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+	int c = 0;
+
+	if (s1 != NULL)
+		c += strlen(s1);
+	if (s2 != NULL)
+		c += strlen(s2);
+	ptr = malloc(sizeof(char) * c + 1);
+	if (ptr == NULL)
+		return (NULL);
+
+	c = 0;
+	if (s1 != NULL)
+	{
+	while (*s1 != '\0')
+	{
+		ptr[c] = *s1;
+		c++;
+	}
+	}
+	if (s2 != NULL)
+	{
+	while (*s2 != '\0')
+	{
+		ptr[c] = *s2;
+		c++;
+	}
+	}
+
+	ptr[c] = '\0';
+	return (ptr);
+}
