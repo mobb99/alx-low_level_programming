8b1ac28bf96f9062cae9a714fb172477003a2774
mobb99 <mobb99@proton.me>
2023-10-04 17:07:35 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	*ptr = malloc(sizeof(int[height][width]));
+	ptr = (int **) malloc(sizeof(int) * heigth * width);
 	if (ptr == NULL)
 		return (NULL);
 
