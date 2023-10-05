4cb4ee2a2fc65b0cfb0cc81ea3745a07fcf39cb1
mobb99 <mobb99@proton.me>
2023-10-04 17:28:59 -0700

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * free_grid - frees a 2 dimensional grid created by alloc_grid
+ * @grid : the grid int **
+ * @height :first dimension
+ *
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+	free(grid[i]);
+	}
+	free(grid);
+}
